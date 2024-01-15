1ec865cde76ba8ec0c01d05d922824a9e91d9406
aleannab <aleannab@gmail.com>
2024-01-14 20:27:48 -0800

    Create multiple blob layers

--- 2024-01-12_lava-lamp/sketch.js
+++ 2024-01-12_lava-lamp/sketch.js
@@ -3,155 +3,180 @@
 // Based off Patt Vira's p5.js Coding Tutorial | Lava Lamp (Marching Squares)
 // https://www.youtube.com/watch?v=lEiXBA_xHOQ
 
-let val;
-
-let cols;
-let rows;
-let size = 10;
-let grid = [];
-
-let circles = [];
-let num = 50; // Number of circles
-
+let gLavaBlobs = [];
+let gHue;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  cols = width / size + 1;
-  rows = height / size + 1;
+  colorMode(HSB);
 
-  for (let i = 0; i < cols; i++) {
-    grid[i] = [];
-    for (let j = 0; j < rows; j++) {
-      grid[i][j] = 0;
-    }
-  }
-
-  for (let i = 0; i < num; i++) {
-    circles[i] = new Circle();
-  }
   strokeWeight(0.5);
+  gHue = random(0, 360);
+
+  let count = 5;
+  for (let i = 0; i < count; i++) {
+    gLavaBlobs.push(new Lava(i / (count - 1)));
+  }
 }
 
 function draw() {
-  background(220);
+  background(0);
 
-  // Calculate values for each grid cell based on circle positions
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      let val = 0;
-      for (let k = 0; k < num; k++) {
-        val +=
-          (circles[k].r * circles[k].r) /
-          ((i * size - circles[k].x) * (i * size - circles[k].x) + (j * size - circles[k].y) * (j * size - circles[k].y));
+  for (let lava of gLavaBlobs) {
+    lava.draw();
+  }
+}
+
+class Lava {
+  constructor(per) {
+    this.weight = map(per, 0, 1, 1, 6);
+    let hue = (gHue + 50 * per) % 360;
+    this.color = color(hue, 100, map(per, 0, 1, 50, 100));
+    this.cols;
+    this.rows;
+    this.size = 15;
+    this.grid = [];
+
+    this.circles = [];
+    let min = map(per, 0, 1, 20, 50);
+    let max = map(per, 0, 1, 50, 100);
+    this.num = 1000 / min;
+
+    this.cols = width / this.size + 1;
+    this.rows = height / this.size + 1;
+
+    for (let i = 0; i < this.cols; i++) {
+      this.grid[i] = [];
+      for (let j = 0; j < this.rows; j++) {
+        this.grid[i][j] = 0;
       }
-      grid[i][j] = val;
+    }
 
-      fill(255);
+    for (let i = 0; i < this.num; i++) {
+      this.circles[i] = new Circle(min, max);
     }
   }
 
-  // Move circles and display them
-  for (let i = 0; i < num; i++) {
-    circles[i].move();
-  }
+  draw() {
+    stroke(this.color);
+    strokeWeight(this.weight);
+    // Calculate values for each grid cell based on circle positions
+    for (let i = 0; i < this.cols; i++) {
+      for (let j = 0; j < this.rows; j++) {
+        let val = 0;
+        for (let k = 0; k < this.num; k++) {
+          val +=
+            (this.circles[k].r * this.circles[k].r) /
+            ((i * this.size - this.circles[k].x) * (i * this.size - this.circles[k].x) +
+              (j * this.size - this.circles[k].y) * (j * this.size - this.circles[k].y));
+        }
+        this.grid[i][j] = val;
+      }
+    }
 
-  // Draw lines connecting points based on grid values
-  for (let i = 0; i < cols - 1; i++) {
-    for (let j = 0; j < rows - 1; j++) {
-      let [a, b, c, d] = [0, 0, 0, 0];
-      let f_a = grid[i][j];
-      let f_b = grid[i + 1][j];
-      let f_c = grid[i + 1][j + 1];
-      let f_d = grid[i][j + 1];
+    // Move circles and display them
+    for (let i = 0; i < this.num; i++) {
+      this.circles[i].move();
+    }
 
-      // Convert grid values to binary based on a threshold
-      a = f_a >= 1 ? 1 : 0;
-      b = f_b >= 1 ? 1 : 0;
-      c = f_c >= 1 ? 1 : 0;
-      d = f_d >= 1 ? 1 : 0;
+    // Draw lines connecting points based on grid values
+    for (let i = 0; i < this.cols - 1; i++) {
+      for (let j = 0; j < this.rows - 1; j++) {
+        let [a, b, c, d] = [0, 0, 0, 0];
+        let f_a = this.grid[i][j];
+        let f_b = this.grid[i + 1][j];
+        let f_c = this.grid[i + 1][j + 1];
+        let f_d = this.grid[i][j + 1];
 
-      let config = 8 * a + 4 * b + 2 * c + 1 * d;
+        // Convert grid values to binary based on a threshold
+        a = f_a >= 1 ? 1 : 0;
+        b = f_b >= 1 ? 1 : 0;
+        c = f_c >= 1 ? 1 : 0;
+        d = f_d >= 1 ? 1 : 0;
 
-      // Calculate intermediate points for line drawing
-      let pt1 = createVector();
-      let amt = (1 - f_a) / (f_b - f_a);
-      pt1.x = lerp(i * size, i * size + size, amt);
-      pt1.y = j * size;
+        let config = 8 * a + 4 * b + 2 * c + 1 * d;
 
-      let pt2 = createVector();
-      amt = (1 - f_b) / (f_c - f_b);
-      pt2.x = i * size + size;
-      pt2.y = lerp(j * size, j * size + size, amt);
+        // Calculate intermediate points for line drawing
+        let pt1 = createVector();
+        let amt = (1 - f_a) / (f_b - f_a);
+        pt1.x = lerp(i * this.size, i * this.size + this.size, amt);
+        pt1.y = j * this.size;
 
-      let pt3 = createVector();
-      amt = (1 - f_d) / (f_c - f_d);
-      pt3.x = lerp(i * size, i * size + size, amt);
-      pt3.y = j * size + size;
+        let pt2 = createVector();
+        amt = (1 - f_b) / (f_c - f_b);
+        pt2.x = i * this.size + this.size;
+        pt2.y = lerp(j * this.size, j * this.size + this.size, amt);
 
-      let pt4 = createVector();
-      amt = (1 - f_a) / (f_d - f_a);
-      pt4.x = i * size;
-      pt4.y = lerp(j * size, j * size + size, amt);
+        let pt3 = createVector();
+        amt = (1 - f_d) / (f_c - f_d);
+        pt3.x = lerp(i * this.size, i * this.size + this.size, amt);
+        pt3.y = j * this.size + this.size;
 
-      // Draw lines based on binary configuration
-      switch (config) {
-        case 0:
-          break;
-        case 1:
-          line(pt3.x, pt3.y, pt4.x, pt4.y);
-          break;
-        case 2:
-          line(pt2.x, pt2.y, pt3.x, pt3.y);
-          break;
-        case 3:
-          line(pt2.x, pt2.y, pt4.x, pt4.y);
-          break;
-        case 4:
-          line(pt1.x, pt1.y, pt2.x, pt2.y);
-          break;
-        case 5:
-          line(pt1.x, pt1.y, pt4.x, pt4.y);
-          line(pt2.x, pt2.y, pt3.x, pt3.y);
-          break;
-        case 6:
-          line(pt1.x, pt1.y, pt3.x, pt3.y);
-          break;
-        case 7:
-          line(pt1.x, pt1.y, pt4.x, pt4.y);
-          break;
-        case 8:
-          line(pt1.x, pt1.y, pt4.x, pt4.y);
-          break;
-        case 9:
-          line(pt1.x, pt1.y, pt3.x, pt3.y);
-          break;
-        case 10:
-          line(pt1.x, pt1.y, pt2.x, pt2.y);
-          line(pt3.x, pt3.y, pt4.x, pt4.y);
-          break;
-        case 11:
-          line(pt1.x, pt1.y, pt2.x, pt2.y);
-          break;
-        case 12:
-          line(pt2.x, pt2.y, pt4.x, pt4.y);
-          break;
-        case 13:
-          line(pt2.x, pt2.y, pt3.x, pt3.y);
-          break;
-        case 14:
-          line(pt3.x, pt3.y, pt4.x, pt4.y);
-          break;
-        case 15:
-          break;
+        let pt4 = createVector();
+        amt = (1 - f_a) / (f_d - f_a);
+        pt4.x = i * this.size;
+        pt4.y = lerp(j * this.size, j * this.size + this.size, amt);
+
+        // Draw lines based on binary configuration
+        switch (config) {
+          case 0:
+            break;
+          case 1:
+            line(pt3.x, pt3.y, pt4.x, pt4.y);
+            break;
+          case 2:
+            line(pt2.x, pt2.y, pt3.x, pt3.y);
+            break;
+          case 3:
+            line(pt2.x, pt2.y, pt4.x, pt4.y);
+            break;
+          case 4:
+            line(pt1.x, pt1.y, pt2.x, pt2.y);
+            break;
+          case 5:
+            line(pt1.x, pt1.y, pt4.x, pt4.y);
+            line(pt2.x, pt2.y, pt3.x, pt3.y);
+            break;
+          case 6:
+            line(pt1.x, pt1.y, pt3.x, pt3.y);
+            break;
+          case 7:
+            line(pt1.x, pt1.y, pt4.x, pt4.y);
+            break;
+          case 8:
+            line(pt1.x, pt1.y, pt4.x, pt4.y);
+            break;
+          case 9:
+            line(pt1.x, pt1.y, pt3.x, pt3.y);
+            break;
+          case 10:
+            line(pt1.x, pt1.y, pt2.x, pt2.y);
+            line(pt3.x, pt3.y, pt4.x, pt4.y);
+            break;
+          case 11:
+            line(pt1.x, pt1.y, pt2.x, pt2.y);
+            break;
+          case 12:
+            line(pt2.x, pt2.y, pt4.x, pt4.y);
+            break;
+          case 13:
+            line(pt2.x, pt2.y, pt3.x, pt3.y);
+            break;
+          case 14:
+            line(pt3.x, pt3.y, pt4.x, pt4.y);
+            break;
+          case 15:
+            break;
+        }
       }
     }
   }
 }
 
 class Circle {
-  constructor() {
+  constructor(min, max) {
     this.x = random(0, width);
     this.y = random(0, height);
-    this.r = random(20, 50);
+    this.r = random(min, max);
     this.dx = random(-2, 2);
     this.dy = random(-2, 2);
   }
