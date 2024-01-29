ac45b41231c4b9a6e416638677189db924aa2f36
aleannab <aleannab@gmail.com>
2024-01-28 22:22:04 -0800

    Create spiderweb like effect

--- 2024-01-20_generative-typography/sketch.js
+++ 2024-01-20_generative-typography/sketch.js
@@ -1,9 +1,11 @@
 // Created for the #Genuary2024 -
 // https://genuary.art/prompts#jan
+
 let font;
-let points = [];
-let r = 15;
-let angle = 0;
+let word = 'GENUARY 2024';
+let textSize = 200;
+let gThreshold = 1;
+let gStartX, gStartY;
 
 function preload() {
   font = loadFont('Roboto-Bold.ttf');
@@ -12,27 +14,66 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  // Calculate the width and height of the text
-  let textWidth = font.textBounds('GENUARY', 0, 0, 300).w;
-  let textHeight = font.textBounds('GENUARY', 0, 0, 300).h;
+  stroke(255);
+  noFill();
+  strokeWeight(0.5);
 
-  // Calculate the starting position to center the text both horizontally and vertically
-  let startX = (width - textWidth) / 2;
-  let startY = (height + textHeight) / 2;
-
-  // Generate points for the centered text
-  points = font.textToPoints('GENUARY', startX, startY, 300, {
-    sampleFactor: 0.1,
-    simplifyThreshold: 0,
-  });
-
-  angleMode(DEGREES);
+  // Calculate text bounds once in setup
+  let textBounds = font.textBounds(word, 0, 0, textSize);
+  gStartX = 0; //textBounds.w; //(width - textBounds.w) / 2;
+  gStartY = textBounds.h; // (height + textBounds.h) / 2;
 }
 
 function draw() {
-  background(220);
-  for (let i = 0; i < points.length; i++) {
-    ellipse(points[i].x, points[i].y, 10, 10);
+  background(0);
+
+  let t = millis() / 2000;
+  for (let i = 0; i < 10; i++) {
+    let sample = abs(0.1 * sin(t));
+    gThreshold = textSize * (0.05 + 0.01 * i);
+
+    let xp = gStartX;
+    let yp = gStartY;
+    for (let i = 0; i < word.length; i++) {
+      let letter = word.charAt(i);
+      let points = font.textToPoints(letter, xp, yp, textSize, {
+        sampleFactor: sample,
+        simplifyThreshold: 0,
+      });
+
+      points.sort((a, b) => a.y - b.y);
+
+      let groupedArrays = {};
+
+      points.forEach((point) => {
+        let roundedY = floor(point.y / gThreshold);
+        groupedArrays[roundedY] = groupedArrays[roundedY] || [];
+        groupedArrays[roundedY].push(point);
+      });
+
+      let separatedArrays = Object.values(groupedArrays);
+
+      separatedArrays.forEach((groupArray) => {
+        groupArray.sort((a, b) => a.x - b.x);
+        beginShape();
+        for (let i = 0; i < groupArray.length; i++) {
+          if (i % 2 === 0) {
+            beginShape();
+            vertex(groupArray[i].x, groupArray[i].y);
+          } else {
+            vertex(groupArray[i].x, groupArray[i].y);
+            endShape();
+          }
+          circle(groupArray[i].x, groupArray[i].y, 2);
+        }
+        endShape();
+      });
+
+      xp += font.textBounds(letter, 0, 0, textSize).w + 10;
+      if (xp > width) {
+        xp = gStartX;
+        yp += gStartY;
+      }
+    }
   }
-  angle += 10;
 }
