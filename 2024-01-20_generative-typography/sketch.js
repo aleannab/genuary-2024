19ee5a7ba0721abc4a6215e96dd2185ccf2a64e2
aleannab <aleannab@gmail.com>
2024-01-28 22:34:46 -0800

    Create character class

--- 2024-01-20_generative-typography/sketch.js
+++ 2024-01-20_generative-typography/sketch.js
@@ -2,10 +2,10 @@
 // https://genuary.art/prompts#jan
 
 let font;
-let word = 'GENUARY 2024';
-let textSize = 200;
-let gThreshold = 1;
+let word = 'GENUARY2024';
+let gTextSize = 200;
 let gStartX, gStartY;
+let gCharacters = [];
 
 function preload() {
   font = loadFont('Roboto-Bold.ttf');
@@ -19,9 +19,21 @@ function setup() {
   strokeWeight(0.5);
 
   // Calculate text bounds once in setup
-  let textBounds = font.textBounds(word, 0, 0, textSize);
+  let textBounds = font.textBounds(word, 0, 0, gTextSize);
   gStartX = 0; //textBounds.w; //(width - textBounds.w) / 2;
   gStartY = textBounds.h; // (height + textBounds.h) / 2;
+
+  let xp = 0;
+  let yp = textBounds.h;
+  for (let i = 0; i < word.length; i++) {
+    let letter = word.charAt(i);
+    gCharacters.push(new Character(letter, xp, yp));
+    xp += font.textBounds(letter, 0, 0, gTextSize).w + 10;
+    if (xp > width) {
+      xp = gStartX;
+      yp += gStartY;
+    }
+  }
 }
 
 function draw() {
@@ -30,50 +42,60 @@ function draw() {
   let t = millis() / 2000;
   for (let i = 0; i < 10; i++) {
     let sample = abs(0.1 * sin(t));
-    gThreshold = textSize * (0.05 + 0.01 * i);
+    let threshold = gTextSize * (0.05 + 0.01 * i);
 
-    let xp = gStartX;
-    let yp = gStartY;
-    for (let i = 0; i < word.length; i++) {
-      let letter = word.charAt(i);
-      let points = font.textToPoints(letter, xp, yp, textSize, {
-        sampleFactor: sample,
-        simplifyThreshold: 0,
-      });
+    for (let c of gCharacters) {
+      c.update(sample, threshold);
+      c.draw();
+    }
+  }
+}
 
-      points.sort((a, b) => a.y - b.y);
+class Character {
+  constructor(c, xp, yp) {
+    this.c = c;
+    this.xp = xp;
+    this.yp = yp;
+    this.rows = [];
+  }
 
-      let groupedArrays = {};
+  update(sample, threshold) {
+    let points = font.textToPoints(this.c, this.xp, this.yp, gTextSize, {
+      sampleFactor: sample,
+      simplifyThreshold: 0,
+    });
 
-      points.forEach((point) => {
-        let roundedY = floor(point.y / gThreshold);
-        groupedArrays[roundedY] = groupedArrays[roundedY] || [];
-        groupedArrays[roundedY].push(point);
-      });
+    points.sort((a, b) => a.y - b.y);
 
-      let separatedArrays = Object.values(groupedArrays);
+    let groups = {};
 
-      separatedArrays.forEach((groupArray) => {
-        groupArray.sort((a, b) => a.x - b.x);
-        beginShape();
-        for (let i = 0; i < groupArray.length; i++) {
-          if (i % 2 === 0) {
-            beginShape();
-            vertex(groupArray[i].x, groupArray[i].y);
-          } else {
-            vertex(groupArray[i].x, groupArray[i].y);
-            endShape();
-          }
-          circle(groupArray[i].x, groupArray[i].y, 2);
+    for (let pt of points) {
+      let roundedY = floor(pt.y / threshold);
+      groups[roundedY] = groups[roundedY] || [];
+      groups[roundedY].push(pt);
+    }
+
+    this.rows = Object.values(groups);
+
+    for (let row of this.rows) {
+      row.sort((a, b) => a.x - b.x);
+    }
+  }
+
+  draw() {
+    for (let row of this.rows) {
+      beginShape();
+      for (let i = 0; i < row.length; i++) {
+        if (i % 2 === 0) {
+          beginShape();
+          vertex(row[i].x, row[i].y);
+        } else {
+          vertex(row[i].x, row[i].y);
+          endShape();
         }
-        endShape();
-      });
-
-      xp += font.textBounds(letter, 0, 0, textSize).w + 10;
-      if (xp > width) {
-        xp = gStartX;
-        yp += gStartY;
+        circle(row[i].x, row[i].y, 2);
       }
+      endShape();
     }
   }
 }
