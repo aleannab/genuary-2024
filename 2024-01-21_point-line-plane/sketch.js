253f2ae083238fed46b1904299cb5660a64e8747
aleannab <aleannab@gmail.com>
2024-02-02 20:05:45 -0800

    Huge refactor

--- 2024-01-21_point-line-plane/sketch.js
+++ 2024-01-21_point-line-plane/sketch.js
@@ -1,118 +1,104 @@
 // Created for the #Genuary2024 -
 // https://genuary.art/prompts#jan
 
-let path = [];
-let ellipses = [];
-let zigZagPoints = [];
-let t = 0;
+let gCurvyLinePath = [];
+let gZigZagPath = [];
 
-let minX, maxX, minY, maxY;
+let gMin, gMax;
 
 function setup() {
   let l = windowWidth > windowHeight ? windowHeight : windowWidth;
   createCanvas(0.9 * l, 0.9 * l);
-
   noFill();
-  stroke(0);
 
-  let marginX = 0.1 * width;
-  minX = marginX;
-  maxX = width - marginX;
+  gMin = createVector(0.1 * width, 0.2 * height);
+  gMax = createVector(0.9 * width, 0.8 * height);
+  noiseDetail(1, 0.25);
 
-  let marginY = 0.2 * height;
-  minY = marginY;
-  maxY = height - marginY;
-
-  // Define a wavy bezier path
-  let count = 50;
-  let inc = (0.8 * width) / (count - 1);
-
-  path.push(createVector(minX + random(0.1, 0.5) * width, maxY));
-  path.push(createVector(minX, random(minY, maxY)));
-  for (let i = 1; i < count; i++) {
-    if (random() > 0.7) continue;
-    let xp = constrain(i * inc + 0.25 * width * random(-1, 1), minX, maxX);
-    path.push(createVector(xp, random(minY, maxY)));
-  }
-  path.push(createVector(maxX, minY));
-
-  // Generate ellipses along the path
-  let eCount = 3000;
-  let eInc = 1 / eCount;
-  for (let i = 0; i < eCount; i++) {
-    let pt = getPointOnBezier(i * eInc, path);
-    ellipses.push(pt);
-  }
-
-  // Calculate zigzag points
-  let zigZagIndex = floor(random(0.5) * ellipses.length);
-  let zigZagCount = 10;
-  let sign = 1;
-  for (let i = 0; i < zigZagCount; i++) {
-    if (zigZagIndex >= ellipses.length) break;
-    let xp = ellipses[zigZagIndex].x;
-    let yp = ellipses[zigZagIndex].y;
-
-    let offset = sign * randomGaussian(height / 5, 30); // randomGaussian(10, 50);
-
-    // Calculate tangent at the current point
-    let tangent = createVector(1, 0);
-    tangent.rotate(atan2(yp - ellipses[zigZagIndex - 1].y, xp - ellipses[zigZagIndex - 1].x));
-
-    // Calculate perpendicular vector
-    let perpendicular = createVector(-tangent.y, tangent.x);
-
-    // Normalize and apply offset
-    perpendicular.normalize();
-    perpendicular.mult(offset);
-
-    // Create zig-zag point
-    let zigZagPoint = createVector(xp, yp).add(perpendicular);
-    zigZagPoint.x = constrain(zigZagPoint.x, minX, maxX);
-    zigZagPoint.y = constrain(zigZagPoint.y, minY, maxY);
-
-    zigZagPoints.push(zigZagPoint);
-    zigZagIndex += floor(random(0.05, 0.2) * ellipses.length);
-    sign *= -1;
-  }
+  createNewSketch();
 }
 
 function draw() {
-  randomSeed(0);
   background(255);
-  noFill();
-  stroke(0);
-
-  // Draw the wavy bezier path
-  // beginShape();
-  // for (let i = 0; i < path.length; i++) {
-  //   vertex(path[i].x, path[i].y);
-  // }
-  // endShape();
-
-  // Draw ellipses along the path
-  fill(0);
-  noFill();
-  strokeWeight(2);
-  noiseDetail(1, 0.25);
-  for (let i = 0; i < ellipses.length; i++) {
-    let xp = ellipses[i].x;
-    let yp = ellipses[i].y;
-    let val = noise(0.001 * xp, 0.01 * yp);
+  for (let cPt of gCurvyLinePath) {
+    let val = noise(0.001 * cPt.x, 0.01 * cPt.y);
     let weight = map(val, 0, 1, 2, 30);
     strokeWeight(weight);
-    point(xp, yp);
+    point(cPt.x, cPt.y);
   }
 
   beginShape();
   strokeWeight(1.5);
-  for (let zPoint of zigZagPoints) {
+  for (let zPoint of gZigZagPath) {
     vertex(zPoint.x, zPoint.y);
   }
   endShape();
 }
 
-// Function to get a point on the wavy bezier path given a parameter t
+function mouseClicked() {
+  createNewSketch();
+}
+
+function createNewSketch() {
+  let path = createPath();
+  createCurvyLine(path);
+  createZigZag();
+}
+
+function createPath() {
+  let path = [];
+  let count = 50;
+  let inc = (0.8 * width) / (count - 1);
+  path.push(createVector(gMin.x + random(0.1, 0.5) * width, gMax.y));
+  path.push(createVector(gMin.x, random(gMin.y, gMax.y)));
+  for (let i = 1; i < count; i++) {
+    if (random() > 0.7) continue;
+    let xp = constrain(i * inc + 0.25 * width * random(-1, 1), gMin.x, gMax.x);
+    path.push(createVector(xp, random(gMin.y, gMax.y)));
+  }
+  path.push(createVector(gMax.x, gMin.y));
+  return path;
+}
+
+function createCurvyLine(path) {
+  gCurvyLinePath = [];
+  let eCount = 3000;
+  let eInc = 1 / eCount;
+  for (let i = 0; i < eCount; i++) {
+    let pt = getPointOnBezier(i * eInc, path);
+    gCurvyLinePath.push(pt);
+  }
+}
+
+function createZigZag() {
+  gZigZagPath = [];
+  let zzIndex = floor(random(0.5) * gCurvyLinePath.length);
+  let sign = 1;
+  for (let i = 0; i < 10; i++) {
+    if (zzIndex >= gCurvyLinePath.length) break;
+    let xp = gCurvyLinePath[zzIndex].x;
+    let yp = gCurvyLinePath[zzIndex].y;
+
+    let offset = sign * randomGaussian(height / 5, 30);
+
+    let tangent = createVector(1, 0);
+    tangent.rotate(atan2(yp - gCurvyLinePath[zzIndex - 1].y, xp - gCurvyLinePath[zzIndex - 1].x));
+
+    let perpendicular = createVector(-tangent.y, tangent.x);
+    perpendicular.normalize();
+    perpendicular.mult(offset);
+
+    let zzPt = createVector(xp, yp).add(perpendicular);
+    zzPt.x = constrain(zzPt.x, gMin.x, gMax.x);
+    zzPt.y = constrain(zzPt.y, gMin.y, gMax.y);
+
+    gZigZagPath.push(zzPt);
+    zzIndex += floor(random(0.05, 0.2) * gCurvyLinePath.length);
+    sign *= -1;
+  }
+}
+
+// Following functions made with ChatGPT
 function getPointOnBezier(t, points) {
   let n = points.length - 1;
   let x = 0;
@@ -127,29 +113,14 @@ function getPointOnBezier(t, points) {
   return createVector(x, y);
 }
 
-// Function to calculate the derivative of a cubic Bezier curve at a specific parameter t
-function getTangentOnBezier(t, points) {
-  let mt = 1 - t;
-  let mt2 = mt * mt;
-  let t2 = t * t;
-
-  let tangentX = -3 * mt2 * points[0].x + 3 * (mt2 - 2 * t * mt) * points[1].x + 3 * (2 * t * mt - t2) * points[2].x + 3 * t2 * points[3].x;
-  let tangentY = -3 * mt2 * points[0].y + 3 * (mt2 - 2 * t * mt) * points[1].y + 3 * (2 * t * mt - t2) * points[2].y + 3 * t2 * points[3].y;
-
-  return createVector(tangentX, tangentY).normalize();
-}
-
-// Bernstein basis function
 function bernstein(n, i, t) {
   return binomialCoefficient(n, i) * pow(t, i) * pow(1 - t, n - i);
 }
 
-// Binomial coefficient function
 function binomialCoefficient(n, k) {
   return factorial(n) / (factorial(k) * factorial(n - k));
 }
 
-// Factorial function
 function factorial(n) {
   if (n === 0 || n === 1) {
     return 1;
